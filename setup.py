024f97fd1e22fa368a59916659393da7bc5fd2cb
Admir Obralija <admir@live.de>
2021-04-13 16:24:51 +0200

    Update project requirements.

--- setup.py
+++ setup.py
@@ -3,11 +3,20 @@ from setuptools import find_packages, setup
 setup(
     name="pusion",
     packages=find_packages(exclude=['tests']),
-    version="1.3.3",
+    version="1.3.4",
     description="A python framework for combining multi-classifier decisions in classification problems",
     author="Admir Obralija",
     license="MIT",
-    install_requires=['numpy', 'scipy', 'sklearn'],
+    install_requires=[
+        'numpy~=1.20.2',
+        'scipy~=1.6.2',
+        'sklearn~=0.0',
+        'pusion~=1.3.3',
+        'scikit-learn~=0.24.1',
+        'pandas~=1.2.3',
+        'matplotlib~=3.4.1',
+        'setuptools~=54.2.0'
+    ],
     setup_requires=['pytest-runner'],
     tests_require=['pytest'],
     test_suite="tests"
