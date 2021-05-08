f309e5f7b6f0669a632b1d45d7cf793008c2e822
Admir Obralija <admir@live.de>
2021-05-08 05:12:27 +0200

    Remove versions from package requirements temporarily.

--- setup.py
+++ setup.py
@@ -8,12 +8,12 @@ setup(
     author="Admir Obralija",
     license="MIT",
     install_requires=[
-        'numpy>=1.20.2',
-        'scipy>=1.6.2',
-        'scikit-learn>=0.24.1',
-        'pandas>=1.2.3',
-        'matplotlib>=3.4.1',
-        'setuptools>=54.2.0'
+        'numpy',
+        'scipy',
+        'scikit-learn',
+        'pandas',
+        'matplotlib',
+        'setuptools'
     ],
     setup_requires=['pytest-runner'],
     tests_require=['pytest'],
