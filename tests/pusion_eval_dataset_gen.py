e871ce8517199f0199d2e948dec84598016f59fa
Admir Obralija <admir@live.de>
2021-05-20 11:01:54 +0200

    Add additional classifiers.

--- tests/pusion_eval_dataset_gen.py
+++ tests/pusion_eval_dataset_gen.py
@@ -1,7 +1,16 @@
 import numpy as np
+from sklearn.dummy import DummyClassifier
 from sklearn.ensemble import RandomForestClassifier
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
+from sklearn.discriminant_analysis import QuadraticDiscriminantAnalysis
+from sklearn.gaussian_process import GaussianProcessClassifier
+from sklearn.gaussian_process.kernels import RBF
+from sklearn.linear_model import LogisticRegression
+from sklearn.naive_bayes import GaussianNB
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.neural_network import MLPClassifier
+from sklearn.svm import SVC
+from sklearn.tree import DecisionTreeClassifier
 
 from pusion import *
 from pusion.input_output.file_input_output import *
@@ -15,11 +24,30 @@ np.random.seed(random_state)
 # ======================================================================================================================
 
 classifiers = [
-    MLPClassifier(max_iter=5000, random_state=1),
-    MLPClassifier(max_iter=5000, random_state=2),
-    MLPClassifier(max_iter=5000, random_state=3),
-    MLPClassifier(max_iter=5000, random_state=4),
-    MLPClassifier(max_iter=5000, random_state=5),
+    MLPClassifier(max_iter=5000, random_state=1),  # MLK
+    MLPClassifier(max_iter=5000, random_state=2),  # MLK
+    MLPClassifier(max_iter=5000, random_state=3),  # MLK
+    MLPClassifier(max_iter=5000, random_state=4),  # MLK
+    MLPClassifier(max_iter=5000, random_state=5),  # MLK
+    # KNeighborsClassifier(1),
+    # KNeighborsClassifier(3),
+    # KNeighborsClassifier(5),
+    # KNeighborsClassifier(7),
+    # KNeighborsClassifier(9),
+    # DecisionTreeClassifier(max_depth=3),  # MLK
+    # RandomForestClassifier(max_depth=5, n_estimators=10, max_features=1),  # MLK
+    # LinearDiscriminantAnalysis(),
+    # LogisticRegression(),
+    # SVC(kernel="linear"),
+    # SVC(kernel="poly"),
+    # SVC(kernel="rbf"),
+    # SVC(kernel="sigmoid"),
+    # SVC(gamma=2, C=1),
+    # GaussianProcessClassifier(1.0 * RBF(1.0)),
+    # GaussianNB(),
+    # QuadraticDiscriminantAnalysis(),
+    # AdaBoostClassifier(n_estimators=40),
+    # DummyClassifier(strategy='uniform'),
 ]
 
 np.random.seed(random_state)
