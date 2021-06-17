0edc3fb6c939eb61362ffa7642ba2dfbeea41cbc
Admir Obralija <admir@live.de>
2021-06-17 21:37:57 +0200

    Delete output.py.

--- pusion/util/output.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-
-
-def print_coverage_matrix(classification_coverage, n_classifiers, n_classes):
-    coverage_matrix = np.zeros((n_classifiers, n_classes), dtype=int)
-    for i in range(len(classification_coverage)):
-        for j in classification_coverage[i]:
-            coverage_matrix[i, j] = 1
-    print(coverage_matrix)
