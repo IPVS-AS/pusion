1c461cdb8ed513f94171c89b6b067613acd87b73
Admir Obralija <admir@live.de>
2021-06-17 05:10:09 +0200

    Update version.

--- setup.py
+++ setup.py
@@ -3,8 +3,8 @@ from setuptools import find_packages, setup
 setup(
     name="pusion",
     packages=find_packages(exclude=['tests']),
-    version="1.3.4",
-    description="A python framework for combining multi-classifier decisions in classification problems",
+    version="1.3.5",
+    description="A python framework for combining multi-classifier decision outputs of classification problems.",
     author="Admir Obralija",
     license="MIT",
     install_requires=[
