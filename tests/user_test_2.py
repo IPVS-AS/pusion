3e387d3b74aa4651cb8513c8e0cc064c28f1de7a
Admir Obralija <admir@live.de>
2021-07-17 18:23:00 +0200

    Add display options.

--- tests/user_test_2.py
+++ tests/user_test_2.py
@@ -1,11 +1,17 @@
 import warnings
 
+import pandas as pd
 from sklearn.neural_network import MLPClassifier
 
 import pusion as p
 from pusion.evaluation.evaluation import Evaluation
 
-warnings.filterwarnings('error')  # halt on error
+# display options
+pd.set_option('display.max_rows', 500)
+pd.set_option('display.max_columns', 500)
+pd.set_option('display.width', 1000)
+# halt on error
+warnings.filterwarnings('error')
 
 
 classifiers = [
